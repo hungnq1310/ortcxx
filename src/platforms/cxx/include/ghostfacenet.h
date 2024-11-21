cff9727eb1ee709a219118d5e6680238ed5bd4ea
hungnq131002 <hungnq131002@gmail.com>
2024-11-21 14:06:38 +0700

    fix: header file of ghostfacenet

--- src/platforms/cxx/include/ghostfacenet.h
+++ src/platforms/cxx/include/ghostfacenet.h
@@ -1,27 +1,22 @@
 #ifndef GHOSTFACENET_H
 #define GHOSTFACENET_H
 
+#include "pipeline.h"
 #include <opencv2/opencv.hpp>
 #include <onnxruntime/core/session/onnxruntime_cxx_api.h>
 #include <vector>
 #include <string>
 
-class GhostFaceNet {
+class GhostFaceNet : public Pipeline {
 public:
-    GhostFaceNet(const std::string& model_path);
-    std::vector<cv::Mat> preprocess(const cv::Mat& image, const std::vector<cv::Rect>& xyxys, const std::vector<std::vector<float>>& kpts);
-    std::vector<float> inference(const cv::Mat& image, const std::vector<cv::Rect>& xyxys, const std::vector<std::vector<float>>& kpts, bool norm = false);
-    void postprocess(const cv::Mat& image);
+    GhostFaceNet(Model model);
+    Ort::Value preprocess(Ort::Value input) override;
+    Ort::Value postprocess(Ort::Value input) override;
+    Ort::Value inference(Ort::Value input) override;
 
 private:
-    Ort::Env env;
-    Ort::SessionOptions session_options;
-    Ort::Session session;
-    std::string input_name;
-    std::string output_name;
-    cv::Size model_input_size;
-
-    Ort::Session load_model(const std::string& path);
+    Ort::Value createOrtValueFromMat(const cv::Mat& mat);
+    cv::Mat createMatFromOrtValue(const Ort::Value& ort_value);
 };
 
 #endif // GHOSTFACENET_H
