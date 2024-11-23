f431756b49211f5520bda6925102da39ecc42d98
hungnq131002 <hungnq131002@gmail.com>
2024-11-24 01:07:18 +0700

    feat: add model wrapper

--- src/headers/model.h
+++ src/headers/model.h
@@ -34,3 +34,67 @@ public:
   ModelOptions(optional<map<string, any>> options, optional<map<string, any>> providers);
   SessionOptions getSessionOptions(optional<map<string, any>> options, optional<map<string, string>> providers);
 };
+
+class Model {
+  protected:
+      std::shared_ptr<Ort::Env> _env;
+      std::shared_ptr<Ort::Allocator> _allocator;
+      std::vector<const char*> inputNames;
+      std::vector<const char*> outputNames;
+      std::unique_ptr<Ort::Session> _session;
+      std::unique_ptr<ModelOptions> _modelOptions;
+      std::string _device;
+      Ort::SessionOptions _sessionOptions;
+      bool isRunned = false;
+
+  public:
+      bool isRunnedModel() {
+          return this->isRunned;
+      };
+
+      Model(
+          const std::string& model, 
+          std::shared_ptr<Ort::Env> env, 
+          std::shared_ptr<Ort::Allocator> allocator, 
+          std::unique_ptr<ModelOptions> _modelOptions,
+          const std::optional<std::map<std::string, std::optional<std::map<std::string, std::string>>>> providers,
+          bool isEncrypted
+      );
+
+      Model(
+          std::string model,
+          std::unique_ptr<ModelOptions> _modelOptions,
+          const std::optional<std::map<std::string, std::optional<std::map<std::string, std::string>>>> providers,
+          bool isEncrypted
+      );
+
+      std::shared_ptr<std::vector<Ort::Value>> run(
+          const std::vector<Ort::Value>& inputs,
+          std::shared_ptr<const char*> outputHead = nullptr,
+          const Ort::RunOptions& runOptions = Ort::RunOptions()
+      );
+      
+      std::future<std::shared_ptr<std::vector<Ort::Value>>> runAsync(
+          const std::vector<Ort::Value>& inputs,
+          std::shared_ptr<const char*> outputHead = nullptr,
+          const Ort::RunOptions runOptions = Ort::RunOptions()
+      );
+
+  protected:
+      Model(
+          std::string model,
+          std::shared_ptr<Ort::Env> env,
+          std::shared_ptr<Ort::Allocator> allocator,
+          std::unique_ptr<ModelOptions> _modelOptions,
+          const std::optional<std::map<std::string, std::optional<std::map<std::string, std::string>>>> providers,
+          bool isEncrypted
+      );
+      Model(
+          std::string model,
+          std::unique_ptr<ModelOptions> _modelOptions,
+          const std::optional<std::map<std::string, std::optional<std::map<std::string, std::string>>>> providers,
+          bool isEncrypted
+      );
+  };
+};
+#endif
