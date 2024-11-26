c92651c388d00d681b469dfc8dd50abe962558aa
hungnq131002 <hungnq131002@gmail.com>
2024-11-26 17:59:11 +0700

    add test wrapper model

--- tests/hello.cpp
+++ tests/hello.cpp
@@ -12,6 +12,53 @@ int main(){
         std::cout << "- " << p << std::endl;
     };
     map<string, any> c;
-    ModelOptions b = ModelOptions(c);
+    c["parallel"] = false;
+    c["inter_ops_threads"] = 1;
+    c["intra_ops_threads"] = 1;
+    c["graph_optimization_level"] = 1;
+
+    // modelNm = "model_convert/extractor.onnx";
+    std::string modelPath = "...";
+    std::string modelName = "extractor.onnx";
+    c["extractor.onnx"] = modelName;
+
+    Model a = Model(modelPath, c, providers, false);
+
+    vector<int64_t> inputShape = {1, 3, 256, 256};
+    vector<float> input_ = vector<float>(1 * 3 * 256 * 256, 1.0);
+
+    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+    Ort::Value inputTensor = Ort::Value::CreateTensor<float>(
+        memory_info, input_.data(), input_.size(), inputShape.data(), inputShape.size()
+    );
+
+    //turn input tensor to vector of Ort::Value
+    vector<Ort::Value> inputs;
+    inputs.push_back(std::move(inputTensor));
+
+    vector<Ort::Value> outputs;
+
+    try {
+        std::shared_ptr<std::vector<Ort::Value>> outputTensors = a.run(
+            inputs,
+            shared_ptr<const char*>(),
+            Ort::RunOptions()
+        );
+
+        std::cout << "Output has : " << outputTensors->size() << " elements\n";
+        for (size_t i = 0; i < outputTensors->size(); ++i) {
+            std::cout << "Head " << i << ": ";
+            auto info = outputTensors->at(i).GetTensorTypeAndShapeInfo();    
+            std::vector<int64_t> tensorShape = info.GetShape();
+            for (int64_t dim : tensorShape) {
+                std::cout << dim << " ";
+            }
+            std::cout << std::endl;
+        }
+
+    } catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+
     return 0;
 }
