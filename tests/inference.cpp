6853a9f4c54f02cce54953b6ab6b5bd795caecb4
hungnq131002 <hungnq131002@gmail.com>
2024-11-27 15:08:26 +0700

    chore: rename file tests, move folder pipeline to modules due to not found reference (CMAKE line 18 only search path in folder modules)

---
+++ tests/inference.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <onnxruntime_cxx_api.h>
+#include <ortcxx/model.h>
+#include <ortcxx/pipeline.h>
+
+using namespace std;
+using namespace ortcxx::model;
+using namespace ortcxx::pipeline;
+
+int main(){
+    std::cout << "Hello, from Cinnamon Runtime!\nAvailable Providers:" << std::endl;
+    auto providers = Ort::GetAvailableProviders();
+    for (std::string p : providers) {
+        std::cout << "- " << p << std::endl;
+    };
+    map<string, any> c;
+    c["parallel"] = false;
+    c["inter_ops_threads"] = 1;
+    c["intra_ops_threads"] = 1;
+    c["graph_optimization_level"] = 1;
+
+    // modelNm = "model_convert/extractor.onnx";
+    std::string modelPath = "...";
+    std::string modelName = "extractor.onnx";
+    c["extractor.onnx"] = modelName;
+
+    Model a = Model(modelPath, c, providers, false);
+
+    vector<int64_t> inputShape = {1, 3, 256, 256};
+    vector<float> input_ = vector<float>(1 * 3 * 256 * 256, 1.0);
+
+    Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtDeviceAllocator, OrtMemTypeCPU);
+    Ort::Value inputTensor = Ort::Value::CreateTensor<float>(
+        memory_info, input_.data(), input_.size(), inputShape.data(), inputShape.size()
+    );
+
+    //turn input tensor to vector of Ort::Value
+    vector<Ort::Value> inputs;
+    inputs.push_back(std::move(inputTensor));
+
+    vector<Ort::Value> outputs;
+
+    Model* pointer_model = &a; 
+    Pipeline p = Pipeline(pointer_model); // Pipeline object
+    //init pointer
+    Ort::Value* input_p = &inputTensor;
+    Ort::Value* output_p = &inputTensor;
+
+
+    // Ort::Value input = p.preprocess(*input_p);
+    // Ort::Value output = p.inference(inputTensor);
+    // Ort::Value result = p.postprocess(output);
+
+    // try {
+    //     std::shared_ptr<std::vector<Ort::Value>> outputTensors = a.run(
+    //         inputs,
+    //         shared_ptr<const char*>(),
+    //         Ort::RunOptions()
+    //     );
+
+    //     std::cout << "Output has : " << outputTensors->size() << " elements\n";
+    //     for (size_t i = 0; i < outputTensors->size(); ++i) {
+    //         std::cout << "Head " << i << ": ";
+    //         auto info = outputTensors->at(i).GetTensorTypeAndShapeInfo();    
+    //         std::vector<int64_t> tensorShape = info.GetShape();
+    //         for (int64_t dim : tensorShape) {
+    //             std::cout << dim << " ";
+    //         }
+    //         std::cout << std::endl;
+    //     }
+
+    // } catch (const std::exception& e) {
+    //     std::cerr << "Exception caught: " << e.what() << std::endl;
+    // }
+
+    return 0;
+}
