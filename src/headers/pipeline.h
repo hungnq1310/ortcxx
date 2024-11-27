3353f9cb61237a027509379dbccd6be156503a6e
hungnq131002 <hungnq131002@gmail.com>
2024-11-27 14:14:55 +0700

    feat: pipeline get pointer of model and add  model attribute; preprocess, postprocess, inference now handle with pointer and address

--- src/headers/pipeline.h
+++ src/headers/pipeline.h
@@ -6,17 +6,19 @@
 #include <onnxruntime_cxx_api.h>
 
 using namespace ortcxx::model;
+using namespace std;
 
 class Pipeline {
     public:
-        Pipeline(Model model);
-        virtual ~Pipeline() = default;
+        Pipeline(Model* model);
+        ~Pipeline();
 
-        virtual Ort::Value preprocess(Ort::Value input);
-        virtual Ort::Value postprocess(Ort::Value input);
-        virtual Ort::Value inference(Ort::Value input);
+        shared_ptr<Ort::Value> preprocess(Ort::Value& input);
+        shared_ptr<Ort::Value> postprocess(Ort::Value& input);
+        shared_ptr<Ort::Value> inference(Ort::Value& input);
 
     protected:
+        Model* model;
         Ort::Env env;
         Ort::SessionOptions session_options;
         Ort::Session session;
