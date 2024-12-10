538a2d7016e173077644fc55d8da30ce483098cf
hungnq131002 <hungnq131002@gmail.com>
2024-12-10 15:18:18 +0700

    chores: using share pointer for Model, add implementation for session clock

--- src/modules/pipeline/pipeline.cpp
+++ src/modules/pipeline/pipeline.cpp
@@ -5,8 +5,9 @@
 using namespace ortcxx::model;
 using namespace ortcxx::pipeline;
 
-Pipeline::Pipeline(Model* model){
+Pipeline::Pipeline(shared_ptr<Model> model){
     this->model = model;
+    // updateSessionClock(); //* CURRENTLY NOT USED
 }
 
 Pipeline::~Pipeline() {
@@ -15,19 +16,50 @@ Pipeline::~Pipeline() {
     this->model = nullptr;
 }
 
-Ort::Value* Pipeline::preprocess(Ort::Value* input) {
-    // Default implementation: return the input as is
-    std::cout << "Preprocessing..." << std::endl;
-    return input;
+
+void Pipeline::updateSessionClock(){
+    std::lock_guard<std::mutex> lock(clockMutex);
+    this->sessionClock = std::chrono::steady_clock::now();
 }
 
-Ort::Value* Pipeline::postprocess(Ort::Value* input) {
-    // Default implementation: return the input as is
-    std::cout << "Postprocessing..." << std::endl;
-    return input;
+float Pipeline::getSessionClock(){
+    std::lock_guard<std::mutex> lock(clockMutex);
+    // auto currentTime = std::chrono::steady_clock::now();
+    auto it = sessionClock.find(model);
+    if (it != sessionClock.end()) {
+        auto duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::steady_clock::now() - it->second).count();
+        // std::cout << "Session duration for model " << model << ": " << duration << " seconds" << std::endl;  // Debugging statement
+        return duration;
+    }
+    // std::cout << "Model " << model << " not found in sessionClock" << std::endl;
+    return 0.0f;
 }
 
-Ort::Value* Pipeline::inference(Ort::Value* input) {
-    // Raise an error if this method is not implemented in a derived class
-    throw std::runtime_error("Inference method not implemented");
+
+void Pipeline::garbageCollector(){
+    while (!stopGCFlag)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5)); // Run every 5ms
+        std::lock_guard<std::mutex> lock(clockMutex);
+        auto currentTime = std::chrono::steady_clock::now();
+       
+        if (std::chrono::duration_cast<std::chrono::milliseconds>(
+            currentTime - this->sessionClock
+        ).count() > this->sessionDuration){
+            this->~Pipeline();
+            
+        }
+    }
+}
+
+void Pipeline::startGC(){
+    if (gc.joinable()){
+        gc.join();
+    }
+    stopGCFlag = false;
+    gc = std::thread(&Pipeline::garbageCollector, this);
+}
+
+void Pipeline::stopGC(){
+    stopGCFlag = true;
 }
