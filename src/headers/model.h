7ad6e259b68939735ff665ca3d49b8f9e5e94107
hungnq131002 <hungnq131002@gmail.com>
2024-12-10 14:19:31 +0700

    style: formatter

--- src/headers/model.h
+++ src/headers/model.h
@@ -36,90 +36,90 @@ public:
 
 class Model {
   protected:
-      std::shared_ptr<Ort::Env> _env;
-      std::shared_ptr<Ort::Allocator> _allocator;
-      std::vector<const char*> inputNames;
-      std::vector<const char*> outputNames;
-      std::unique_ptr<Ort::Session> _session;
-      std::unique_ptr<ModelOptions> _modelOptions;
-      std::string _device;
-      std::unique_ptr<Ort::SessionOptions> _sessionOptions;
-      bool isRunned = false;
+    std::shared_ptr<Ort::Env> _env;
+    std::shared_ptr<Ort::Allocator> _allocator;
+    std::vector<const char*> inputNames;
+    std::vector<const char*> outputNames;
+    std::unique_ptr<Ort::Session> _session;
+    std::unique_ptr<ModelOptions> _modelOptions;
+    std::string _device;
+    std::unique_ptr<Ort::SessionOptions> _sessionOptions;
+    bool isRunned = false;
 
   public:
-      bool isRunnedModel() {
-          return this->isRunned;
-      };
+    bool isRunnedModel() {
+      return this->isRunned;
+    };
 
-      static std::shared_ptr<Model> create(
-        const std::string& modelPath, 
-        std::shared_ptr<Ort::Env> env, 
-        std::shared_ptr<Ort::Allocator> allocator, 
-        const std::optional<std::map<std::string, std::any>> options,
-        const optional<vector<string>> providers,
-        bool isEncrypted
-      ) {
-        return std::shared_ptr<Model>(new Model(modelPath, env, allocator, options, providers, isEncrypted));
-      }
-      static std::shared_ptr<Model> create(
-        const std::string& modelBuffer,
-        size_t modelSize,
-        std::shared_ptr<Ort::Env> env, 
-        std::shared_ptr<Ort::Allocator> allocator, 
-        const std::optional<std::map<std::string, std::any>> options,
-        const optional<vector<string>> providers,
-        bool isEncrypted
-      ) {
-        return std::shared_ptr<Model>(new Model(modelBuffer, modelSize, env, allocator, options, providers, isEncrypted));
-      }
+    static std::shared_ptr<Model> create(
+      const std::string& modelPath, 
+      std::shared_ptr<Ort::Env> env, 
+      std::shared_ptr<Ort::Allocator> allocator, 
+      const std::optional<std::map<std::string, std::any>> options,
+      const optional<vector<string>> providers,
+      bool isEncrypted
+    ) {
+      return std::shared_ptr<Model>(new Model(modelPath, env, allocator, options, providers, isEncrypted));
+    }
 
-      Model(
-        const std::string& modelPath,
-        const std::optional<std::map<std::string, std::any>> options,
-        const optional<vector<string>> providers,
-        bool isEncrypted
-      );
+    static std::shared_ptr<Model> create(
+      const std::string& modelBuffer,
+      size_t modelSize,
+      std::shared_ptr<Ort::Env> env, 
+      std::shared_ptr<Ort::Allocator> allocator, 
+      const std::optional<std::map<std::string, std::any>> options,
+      const optional<vector<string>> providers,
+      bool isEncrypted
+    ) {
+      return std::shared_ptr<Model>(new Model(modelBuffer, modelSize, env, allocator, options, providers, isEncrypted));
+    }
 
-      Model(
-          const std::string& modelBuffer,
-          size_t modelSize,
-          const std::optional<std::map<std::string, std::any>> options,
-          const optional<vector<string>> providers,
-          bool isEncrypted
-      );
+    Model(
+      const std::string& modelPath,
+      const std::optional<std::map<std::string, std::any>> options,
+      const optional<vector<string>> providers,
+      bool isEncrypted
+    );
 
-      std::shared_ptr<std::vector<Ort::Value>> run(
-          const std::vector<Ort::Value>& inputs,
-          std::shared_ptr<const char*> outputHead = nullptr,
-          const Ort::RunOptions& runOptions = Ort::RunOptions()
-      );
-      
-      std::future<std::shared_ptr<std::vector<Ort::Value>>> runAsync(
-          const std::vector<Ort::Value>& inputs,
-          std::shared_ptr<const char*> outputHead = nullptr,
-          const Ort::RunOptions runOptions = Ort::RunOptions()
-      );
+    Model(
+      const std::string& modelBuffer,
+      size_t modelSize,
+      const std::optional<std::map<std::string, std::any>> options,
+      const optional<vector<string>> providers,
+      bool isEncrypted
+    );
 
-  protected:
-  //? why make this protected?
-      Model(
-          std::string modelPath,
-          std::shared_ptr<Ort::Env> env,
-          std::shared_ptr<Ort::Allocator> allocator,
-          const std::optional<std::map<std::string, std::any>> options,
-          const optional<vector<string>> providers,
-          bool isEncrypted
-      );
+    Model(
+      std::string modelPath,
+      std::shared_ptr<Ort::Env> env,
+      std::shared_ptr<Ort::Allocator> allocator,
+      const std::optional<std::map<std::string, std::any>> options,
+      const optional<vector<string>> providers,
+      bool isEncrypted
+    );
 
-      Model(
-          const std::string& modelBuffer,
-          size_t modelSize,
-          std::shared_ptr<Ort::Env> env,
-          std::shared_ptr<Ort::Allocator> allocator,
-          const std::optional<std::map<std::string, std::any>> options,
-          const optional<vector<string>> providers,
-          bool isEncrypted
-      );
+    Model(
+      const std::string& modelBuffer,
+      size_t modelSize,
+      std::shared_ptr<Ort::Env> env,
+      std::shared_ptr<Ort::Allocator> allocator,
+      const std::optional<std::map<std::string, std::any>> options,
+      const optional<vector<string>> providers,
+      bool isEncrypted
+    );
+
+    std::shared_ptr<std::vector<Ort::Value>> run(
+      const std::vector<Ort::Value>& inputs,
+      std::shared_ptr<const char*> outputHead = nullptr,
+      const Ort::RunOptions& runOptions = Ort::RunOptions()
+    );
+    
+    std::future<std::shared_ptr<std::vector<Ort::Value>>> runAsync(
+      const std::vector<Ort::Value>& inputs,
+      std::shared_ptr<const char*> outputHead = nullptr,
+      const Ort::RunOptions runOptions = Ort::RunOptions()
+    );
+
   };
 }; // namespace ortcxx::model
 #endif
